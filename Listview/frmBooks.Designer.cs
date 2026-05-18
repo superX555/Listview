dea44f583885998bd053cfa77d4b36b41906290d
LUKE <LUKE@LAPTOP-FJV5DOFD>
2026-05-18 21:11:06 +0800

    加入專案檔案。

---
+++ Listview/frmBooks.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Listview
+{
+    partial class lstBorrow
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lstBorrow));
+            this.lvwBooks = new System.Windows.Forms.ListView();
+            this.imgL = new System.Windows.Forms.ImageList(this.components);
+            this.imgS = new System.Windows.Forms.ImageList(this.components);
+            this.pnlTools = new System.Windows.Forms.Panel();
+            this.grpBorrow = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.grpView = new System.Windows.Forms.GroupBox();
+            this.cmbView = new System.Windows.Forms.ComboBox();
+            this.pnlTools.SuspendLayout();
+            this.grpBorrow.SuspendLayout();
+            this.grpView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvwBooks
+            // 
+            this.lvwBooks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwBooks.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lvwBooks.HideSelection = false;
+            this.lvwBooks.LargeImageList = this.imgL;
+            this.lvwBooks.Location = new System.Drawing.Point(0, 0);
+            this.lvwBooks.Name = "lvwBooks";
+            this.lvwBooks.Size = new System.Drawing.Size(466, 450);
+            this.lvwBooks.StateImageList = this.imgS;
+            this.lvwBooks.TabIndex = 0;
+            this.lvwBooks.UseCompatibleStateImageBehavior = false;
+            this.lvwBooks.ItemActivate += new System.EventHandler(this.lvwBooks_ItemActivate);
+            this.lvwBooks.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imgL
+            // 
+            this.imgL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgL.ImageStream")));
+            this.imgL.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgL.Images.SetKeyName(0, "Book1.bmp");
+            this.imgL.Images.SetKeyName(1, "Book2.bmp");
+            this.imgL.Images.SetKeyName(2, "Book3.bmp");
+            this.imgL.Images.SetKeyName(3, "Book4.bmp");
+            this.imgL.Images.SetKeyName(4, "Book5.bmp");
+            this.imgL.Images.SetKeyName(5, "Book6.bmp");
+            this.imgL.Images.SetKeyName(6, "Book7.bmp");
+            this.imgL.Images.SetKeyName(7, "Book8.bmp");
+            // 
+            // imgS
+            // 
+            this.imgS.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgS.ImageStream")));
+            this.imgS.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgS.Images.SetKeyName(0, "Book1.bmp");
+            this.imgS.Images.SetKeyName(1, "Book2.bmp");
+            this.imgS.Images.SetKeyName(2, "Book3.bmp");
+            this.imgS.Images.SetKeyName(3, "Book4.bmp");
+            this.imgS.Images.SetKeyName(4, "Book5.bmp");
+            this.imgS.Images.SetKeyName(5, "Book6.bmp");
+            this.imgS.Images.SetKeyName(6, "Book7.bmp");
+            this.imgS.Images.SetKeyName(7, "Book8.bmp");
+            // 
+            // pnlTools
+            // 
+            this.pnlTools.Controls.Add(this.grpBorrow);
+            this.pnlTools.Controls.Add(this.grpView);
+            this.pnlTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlTools.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.pnlTools.Location = new System.Drawing.Point(466, 0);
+            this.pnlTools.Name = "pnlTools";
+            this.pnlTools.Size = new System.Drawing.Size(334, 450);
+            this.pnlTools.TabIndex = 1;
+            // 
+            // grpBorrow
+            // 
+            this.grpBorrow.Controls.Add(this.listBox1);
+            this.grpBorrow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpBorrow.Location = new System.Drawing.Point(0, 75);
+            this.grpBorrow.Name = "grpBorrow";
+            this.grpBorrow.Size = new System.Drawing.Size(334, 375);
+            this.grpBorrow.TabIndex = 1;
+            this.grpBorrow.TabStop = false;
+            this.grpBorrow.Text = "借書清單";
+            this.grpBorrow.Enter += new System.EventHandler(this.grpBorrow_Enter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(3, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 338);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // grpView
+            // 
+            this.grpView.Controls.Add(this.cmbView);
+            this.grpView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpView.Location = new System.Drawing.Point(0, 0);
+            this.grpView.Name = "grpView";
+            this.grpView.Size = new System.Drawing.Size(334, 75);
+            this.grpView.TabIndex = 0;
+            this.grpView.TabStop = false;
+            this.grpView.Text = "檢視方式";
+            // 
+            // cmbView
+            // 
+            this.cmbView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbView.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbView.FormattingEnabled = true;
+            this.cmbView.Location = new System.Drawing.Point(6, 32);
+            this.cmbView.Name = "cmbView";
+            this.cmbView.Size = new System.Drawing.Size(322, 37);
+            this.cmbView.TabIndex = 2;
+            this.cmbView.SelectedIndexChanged += new System.EventHandler(this.cmbView_SelectedIndexChanged);
+            // 
+            // lstBorrow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvwBooks);
+            this.Controls.Add(this.pnlTools);
+            this.Name = "lstBorrow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "圖書管理程式";
+            this.Load += new System.EventHandler(this.lstBorrow_Load);
+            this.pnlTools.ResumeLayout(false);
+            this.grpBorrow.ResumeLayout(false);
+            this.grpView.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwBooks;
+        private System.Windows.Forms.Panel pnlTools;
+        private System.Windows.Forms.GroupBox grpBorrow;
+        private System.Windows.Forms.GroupBox grpView;
+        private System.Windows.Forms.ComboBox cmbView;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ImageList imgL;
+        private System.Windows.Forms.ImageList imgS;
+    }
+}
+
---
+++ Listview/frmBooks.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listview
+{
+    public partial class lstBorrow : Form
+    {
+        string[] b_name = { "三國演義", "西遊記","唐詩三百首","楚辭",
+                "西廂記","水滸傳","紅樓夢", "牡丹亭" }; //書名
+        string[] author = {"羅貫中", "吳承恩", "孫洙", "劉向",
+                "王實甫","施耐庵", "曹雪芹", "湯顯祖" }; //作者
+        string[] kind = { "章回小說", "章回小說", "詩選", "詩歌", "戲曲",
+                "章回小說","章回小說", "戲曲" }; //類別
+
+        public lstBorrow()
+        {
+            InitializeComponent();
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void grpBorrow_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void lstBorrow_Load(object sender, EventArgs e)
+        {
+            // 設定檢視方式下拉選單項目
+            cmbView.Items.Add("大圖示");
+            cmbView.Items.Add("詳細資料");
+            cmbView.Items.Add("小圖示");
+            cmbView.Items.Add("清單");
+            cmbView.Items.Add("大圖示加詳細資料");
+            cmbView.SelectedIndex = 0; //預設選取第一個項目 (大圖示)
+
+            // 新增 ListView 欄位
+            lvwBooks.Columns.Add("書名", 130);
+            lvwBooks.Columns.Add("作者", 80);
+            lvwBooks.Columns.Add("類別", 100);
+
+            lvwBooks.BeginUpdate(); //暫停重繪，優化載入效能
+
+            // 跑迴圈載入所有書籍資料
+            for (int i = 0; i < b_name.Length; i++)
+            {
+                ListViewItem lvi = new ListViewItem(b_name[i]);
+                lvi.SubItems.Add(author[i]); //新增 作者 欄位資料
+                lvi.SubItems.Add(kind[i]);   //新增 類別 欄位資料
+                lvwBooks.Items.Add(lvi);     //新增項目
+                lvwBooks.Items[i].ImageIndex = i; //指定影像的索引值
+            }
+
+            lvwBooks.EndUpdate(); //恢復重繪
+        }
+
+        private void cmbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //根據cmbView的SelectedIndex屬性值，設定檢視方式
+            switch (cmbView.SelectedIndex)
+            {
+                case 0: //大圖示
+                    lvwBooks.View = View.LargeIcon;
+                    break;
+                case 1: //詳細資料
+                    lvwBooks.View = View.Details;
+                    break;
+                case 2: //小圖示
+                    lvwBooks.View = View.SmallIcon;
+                    break;
+                case 3: //清單
+                    lvwBooks.View = View.List;
+                    break;
+                case 4: //大圖示加詳細資料
+                    lvwBooks.View = View.Tile;
+                    break;
+            }
+        }
+
+        private void lvwBooks_ItemActivate(object sender, EventArgs e)
+        {
+            // 確保使用者真的有選取項目，避免點擊空白處引發異常
+            if (lvwBooks.SelectedIndices.Count > 0)
+            {
+                //取得選中的書名
+                string strBookname = b_name[lvwBooks.SelectedIndices[0]];
+
+                // 【關鍵修正】：將 lstBorrow.Items 改為 listBox1.Items
+                bool exist = listBox1.Items.Contains(strBookname);
+
+                if (exist != true) // 若選取的書名不存在借書清單中
+                {
+                    DialogResult dr = MessageBox.Show("確定要借閱嗎?",
+                    strBookname, MessageBoxButtons.YesNo);
+
+                    if (dr == DialogResult.Yes) // 若按 <是> 鈕
+                    {
+                        // 【關鍵修正】：將項目新增到 listBox1 控制項中
+                        listBox1.Items.Add(strBookname);
+                    }
+                }
+            }
+        }
+    }
+}
